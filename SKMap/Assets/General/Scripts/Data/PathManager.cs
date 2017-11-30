1486331b8784917d6244c49ec479e50fe9347269
trung <ngoctrungbmt@gmail.com>
2017-11-30 11:20:25 +0700

    first Init

---
+++ SKMap/Assets/General/Scripts/Data/PathManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public static class PathManager
+{
+    private static string _dlc;
+
+    public static string DLC
+    {
+        get
+        {
+            _dlc = Application.persistentDataPath + "/DLC/";
+
+            if (!Directory.Exists(_dlc))
+            {
+                Directory.CreateDirectory(_dlc);
+            }
+            return _dlc;
+        }
+    }
+}
+
+public class GameTags
+{
+    public const string SettingDataKey = "SettingDataKey";
+    public const string UserinfoDataKey = "UserinfoDataKey";
+    public const string UserStatusDataKey = "UserStatusDataKey";
+}
+
+public class SceneName
+{
+    public const string Splash = "Splash";
+    public const string Login = "Login";
+    public const string Home = "Home";
+    public const string Empty = "Empty";
+}
